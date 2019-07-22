31448aa0d4d3b8126f258d86f16b245fdc3769dc
vipulwadhwa <vipulwadhwa2408@gmail.com>
2019-07-22 23:27:05 +0530

    initial commit

---
+++ app/controllers/users/confirmations_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Users::ConfirmationsController < Devise::ConfirmationsController
+  # GET /resource/confirmation/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/confirmation
+  # def create
+  #   super
+  # end
+
+  # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    @resource = resource_class.confirm_by_token(params[:confirmation_token])
+    if @resource
+      Voter.create(user: @resource, precinct: SecureRandom.hex(2), first_name: SecureRandom.hex(2), last_name: SecureRandom.hex(2))
+      redirect_to new_user_session_path
+    end
+  end
+
+  # protected
+
+  # The path used after resending confirmation instructions.
+  # def after_resending_confirmation_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+
+  # The path used after confirmation.
+  # def after_confirmation_path_for(resource_name, resource)
+  #   super(resource_name, resource)
+  # end
+end
