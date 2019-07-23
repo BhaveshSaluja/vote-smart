2db9736cdad174b45f897f63621118a9d8a8386f
vipulwadhwa <vipulwadhwa2408@gmail.com>
2019-07-23 21:06:56 +0530

    fixed test case

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HomeController do
   describe "GET index" do
     it "renders the index template" do
       get :index
-      expect(response).to render_template("index")
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
